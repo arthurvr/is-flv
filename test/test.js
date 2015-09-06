bccf2237a78f96c064e3508cca0d447b2436dc89
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 13:45:13 +0200

    Use `ava` for testing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var readChunk = require('read-chunk');
 var isFlv = require('..');
 
@@ -7,10 +7,10 @@ function check(filename) {
 	return isFlv(readChunk.sync(filename, 0, 4));
 }
 
-it('should return true when passing an flv', function () {
-	assert(check('test/fixture.flv'));
+test('should return true when passing an flv', function (t) {
+	t.true(check('test/fixture.flv'));
 });
 
-it('should return false when not passing an flv', function () {
-	assert(!check('test/fixture.avi'));
+test('should return false when not passing an flv', function (t) {
+	t.false(check('test/fixture.avi'));
 });
