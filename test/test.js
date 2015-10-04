00ee86a22379ac144f1909dfbfac0c690bf279de
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-04 10:46:37 +0200

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
-'use strict';
-var test = require('ava');
-var readChunk = require('read-chunk');
-var isFlv = require('..');
+import test from 'ava';
+import readChunk from 'read-chunk';
+import isFlv from '..';
 
 function check(filename) {
 	return isFlv(readChunk.sync(filename, 0, 4));
 }
 
-test('should return true when passing an flv', function (t) {
+test('should return true when passing an flv', t => {
 	t.true(check('test/fixture.flv'));
 	t.end();
 });
 
-test('should return false when not passing an flv', function (t) {
+test('should return false when not passing an flv', t => {
 	t.false(check('test/fixture.avi'));
 	t.end();
 });
